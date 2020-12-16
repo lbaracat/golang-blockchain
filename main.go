010fa8daa0a4541a217c3f2f7f2dd4d9fb669a15
Baracat <lbaracat@gmail.com>
2020-12-16 00:50:02 -0300

    Very basic blockchain implemented
    Basic, but functional blockchain data structure.

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"fmt"
 )
 
-// Block is the main struct in this blockchain implementation
+// Block is the blockchain unit in this implementation
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+}
+
+// BlockChain struct for this implementation
+type BlockChain struct {
+	blocks []*Block
 }
 
 // DeriveHash method to calculate block's hashes
@@ -26,6 +32,35 @@ func CreateBlock(data string, PrevHash []byte) *Block {
 	return block
 }
 
-func main() {
+// AddBlock call CreateBlock and append this on BlockChain
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
 
+}
+
+// Genesis generates the first block
+func Genesis() *Block {
+	return CreateBlock("In the beginning dev created this block and the blockchain", []byte{})
+}
+
+// InitBlockChain makes the blockchain
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
+func main() {
+	chain := InitBlockChain()
+
+	chain.AddBlock("First block after The Beginning")
+	chain.AddBlock("Second block after The Beginning - third in this blockchain")
+	chain.AddBlock("Fourth block in this blockchain...")
+
+	for k, block := range chain.blocks {
+		fmt.Printf("Block #%d\n", k)
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n\n", block.Hash)
+	}
 }
